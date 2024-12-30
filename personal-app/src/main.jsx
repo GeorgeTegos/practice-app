941990aa2c51b7e063fa90713a5bbfd743b9c693
Tination <avgoustina.pinialidi.2012@gmail.com>
2024-12-30 22:01:27 +0000

    added some structure to main

--- personal-app/src/main.jsx
+++ personal-app/src/main.jsx
@@ -1,8 +1,22 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import NavBar from './components/NavBar';
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import NavBar from "./components/NavBar";
+import MainArticle from "./components/MainArticle";
 
-createRoot(document.getElementById('root')).render(
-  <NavBar />
-)
+createRoot(document.getElementById("root")).render(
+  <>
+    <div id="header-section">
+      <NavBar />
+    </div>
+    <div id="main-section">
+      <div id="container-one">
+        {/* BannersSection */}
+        <div id="banners-section"></div>
+        <MainArticle />
+      </div>
+      <div id="container-two"></div>
+    </div>
+    <div id="footer-section"></div>
+  </>
+);
