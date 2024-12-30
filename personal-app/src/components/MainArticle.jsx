5faf6b92341d75ecfaa84d6a08d31cc5fc4e7273
Tination <avgoustina.pinialidi.2012@gmail.com>
2024-12-30 22:01:06 +0000

    created main article comp

---
+++ personal-app/src/components/MainArticle.jsx
@@ -0,0 +1,43 @@
+function MainArticle() {
+  return (
+    <div id="main-article-section">
+      <h1>Harry Potter Introduction </h1>
+
+      <p>
+        A fictional character, Harry Potter is the boy wizard created by British
+        author J.K. Rowling. His coming-of-age exploits were the subject of
+        seven enormously popular novels. The novels were adapted into eight
+        blockbuster films (the final book was adapted into two films) that
+        debuted between 2001 and 2011. A play and a book of its script, Harry
+        Potter and the Cursed Child, appeared in 2016.
+      </p>
+
+      <ol>
+        <li>
+          Harry Potter and the Philosopher's Stone (1997; also published as
+          Harry Potter and the Sorcerer's Stone)
+        </li>
+        <li>Harry Potter and the Chamber of Secrets (1998)</li>
+        <li>Harry Potter and the Prisoner of Azkaban (1999)</li>
+        <li>Harry Potter and the Goblet of Fire (2000)</li>
+        <li>Harry Potter and the Order of the Phoenix (2003)</li>
+        <li>Harry Potter and the Half-Blood Prince (2005)</li>
+        <li>Harry Potter and the Deathly Hallows (2007)</li>
+      </ol>
+
+      <p>
+        The Harry Potter series was extremely popular with both children and
+        adults worldwide. All the books were best sellers, available in more
+        than 200 countries and some 60 languages. The phenomenon introduced new
+        terms into common usage. For example, muggle became popular as a word
+        for a person who possesses no magical powers. The series of eight films
+        made stars of such actors as Daniel Radcliffe, Rupert Grint, and Emma
+        Watson. Together, the book and movie series spawned a huge amount of
+        related merchandise. Beginning in 2010 Harry Potter attractions opened
+        at Universal Studios theme parks in the United States and Japan.
+      </p>
+    </div>
+  );
+}
+
+export default MainArticle;
